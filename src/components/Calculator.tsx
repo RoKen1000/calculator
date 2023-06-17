ac4e4ecdc8d08ad6aa5faf06974542a77441a7d0
RoKen1000 <rosskennedy993@outlook.com>
2023-06-17 11:45:19 +0100

    Create front-end calculator interface.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -10,7 +10,22 @@ export const Calculator: FC = () => {
     return(
         <main>
             <div className="calculator-box">
-
+                <div className="calculator-display"></div>
+                    <button value={"+"}>+</button>
+                    <button value={"-"}>-</button>
+                    <button value={"*"}>*</button>
+                    <button value={"/"}>/</button>
+                    <button className="equals-button"  >=</button>
+                    <button className="clear-button">x</button>
+                    <button value={7}>7</button>
+                    <button value={8}>8</button>
+                    <button value={9}>9</button>
+                    <button value={4}>4</button>
+                    <button value={5}>5</button>
+                    <button value={6}>6</button>
+                    <button value={1}>1</button>
+                    <button value={2}>2</button>
+                    <button value={3}>3</button>
             </div>
         </main>
     )
