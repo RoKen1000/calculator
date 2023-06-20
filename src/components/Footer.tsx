d3229aaa31474724623a795437fd3c8ff2598938
RoKen1000 <rosskennedy993@outlook.com>
2023-06-20 10:11:56 +0100

    Rectify clumsy wording of text in Footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
 
     return(
         <footer className="footer">
-            <p>Created using TypeScript, React, JSX, and CSS and tested with Jest</p>
+            <p>Created using TypeScript, React, JSX, and CSS. Tested with Jest</p>
             <p>Created by Ross Kennedy</p>
             <p>View my <a href="https://ross-kennedy-dev.netlify.app/">portfolio website here</a></p>
         </footer>
