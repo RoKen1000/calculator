d3029ee12168b93c8d5727d4f81de9f11c5fc3a7
RoKen1000 <rosskennedy993@outlook.com>
2023-06-20 10:21:45 +0100

    Add link to project github repository in Footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ export const Footer = () => {
     return(
         <footer className="footer">
             <p>Created using TypeScript, React, JSX, and CSS. Tested with Jest</p>
+            <p>View the <a href="https://github.com/RoKen1000/typescript-calculator">repository for this project here</a></p>
             <p>Created by Ross Kennedy</p>
             <p>View my <a href="https://ross-kennedy-dev.netlify.app/">portfolio website here</a></p>
         </footer>
